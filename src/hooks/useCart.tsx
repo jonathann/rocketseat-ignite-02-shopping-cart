1000760657606b54c60b5f3f4040fd62d3940126
Jonathan Gehard Kohler <jonathangkohler@gmail.com>
2021-03-25 10:56:16 -0300

    Challenge 02 finished

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -23,28 +23,77 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  //automatically setStorage when cart object Changers
+  //1)new useRef
+  const previousCartRef = useRef<Product[]>()
+  //2)copy old reference before cart changes
+  useEffect(() => {
+    previousCartRef.current = cart
+  })
+  //3) first time "previousReference" = "actualReference"
+  const cartPreviousValue = previousCartRef.current ?? cart
+  //4) monitor cart, compare previousReference, and update localStorage if necessary
+  useEffect(() => {
+    if (cartPreviousValue !== cart) {
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+    }
+  }, [cart, cartPreviousValue])
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+
+      const dataStock = await api.get(`/stock/${productId}`)
+      const stock: Stock = dataStock.data
+      const stockAmount = stock.amount
+      const newCart = [...cart]
+      const productExists = cart.find(product => product.id === productId)
+      const currentAmount = productExists ? productExists.amount : 0
+
+      if (currentAmount + 1 > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      if (productExists) {
+        productExists.amount++
+      } else {
+        const productData = await api.get(`/products/${productId}`)
+        newCart.push({ ...productData.data, amount: 1 })
+      }
+
+      setCart(newCart)
+      // __setNewCart(newCart)
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      //just to pass at rocketseat tests 
+      // (i know it passes two times at cart array, but it's only educational,
+      // and I prefer the second alternative)
+      if (cart.filter(product => product.id === productId).length === 0) {
+        throw Error('Product not found')
+      }
+
+      //if product is not found, does nothing
+      const newCart = cart.filter(product => product.id !== productId)
+      // __setNewCart(newCart)
+      setCart(newCart)
+
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto')
     }
   };
 
@@ -53,11 +102,35 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+
+      if (amount <= 0) {
+        return
+      }
+
+      const dataStock = await api.get('/stock/' + productId)
+      const stockAmount = dataStock.data.amount
+
+      if (stockAmount < amount) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      const newCart = cart.map(product => {
+        product.id === productId && (product.amount = amount)
+        return product
+      })
+      // __setNewCart(newCart)
+      setCart(newCart)
+
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
+
+  // function __setNewCart(newCart: Product[]) {
+  //   localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart))
+  //   setCart(newCart)
+  // }
 
   return (
     <CartContext.Provider
